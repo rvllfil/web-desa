99fddb22b4b02f4e4d4d55b7ce5d2a41db6431da
rvllfil <rullfhiel@gmail.com>
2020-06-24 10:47:29 +0700

    menambahkan partials navbar dan footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,14 @@
+<footer>
+  <div class="container">
+    <div class="footer-info">
+      <p><span class="fa fa-copyright"></span>Pemeritah Desa Cibatu 2020</p>
+      <p>Dikembangkan oleh @rvllfil</p>
+    </div>
+    <div class="socmed-icon">
+      <a href="" class="fa fa-facebook"></a>
+      <a href="" class="fa fa-twitter"></a>
+      <a href="" class="fa fa-youtube"></a>
+      <a href="" class="fa fa-whatsapp"></a>
+    </div>
+  </div>
+</footer>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <div class="container">
+    <a class="navbar-brand" href="#"><img src="{{ asset('img/logo.png') }}" alt="logo" height="50px"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Tentang
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Visi & Misi</a>
+            <a class="dropdown-item" href="#">Wilayah</a>
+            <a class="dropdown-item" href="#">Sejarah</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Lembaga
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Aparat Desa</a>
+            <a class="dropdown-item" href="#">BPD</a>
+            <a class="dropdown-item" href="#">LPM</a>
+            <a class="dropdown-item" href="#">PKK</a>
+            <a class="dropdown-item" href="#">Karang Taruna</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Transparansi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Kabar Desa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Kontak</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
